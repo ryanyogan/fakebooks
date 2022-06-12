5b09423caf4f90debf8ff962b8b7cef6b0d6d6ac
Ryan Yogan <ryanyogan@gmail.com>
2022-06-11 19:39:04 -0500

    deposits look good

--- app/routes/__app/sales/customers/new.tsx
+++ app/routes/__app/sales/customers/new.tsx
@@ -1,11 +1,23 @@
 import type { ActionFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
 import { Response } from "@remix-run/node";
 import { redirect } from "@remix-run/node";
-import { Form } from "@remix-run/react";
+import { Form, useActionData } from "@remix-run/react";
 import invariant from "tiny-invariant";
 import { inputClasses, LabelText, submitButtonClasses } from "~/components";
 import { createCustomer } from "~/models/customer.server";
 import { requireUser } from "~/utils/session.server";
+
+type ActionData = {
+  errors: {
+    email: string | null;
+    name: string | null;
+  };
+};
+
+function validateName(name: string) {
+  return name === "" ? "Please enter a valid name" : null;
+}
 
 export const action: ActionFunction = async ({ request }) => {
   await requireUser(request);
@@ -19,6 +31,19 @@ export const action: ActionFunction = async ({ request }) => {
       invariant(typeof name === "string", "name is required");
       invariant(typeof email === "string", "email is required");
 
+      const errors: ActionData["errors"] = {
+        name: validateName(name),
+        email: validateName(email),
+      };
+
+      const nameHasErrors = errors.name !== null;
+      const emailHasErrors = errors.email !== null;
+      const hasErrors = nameHasErrors || emailHasErrors;
+
+      if (hasErrors) {
+        return json<ActionData>({ errors });
+      }
+
       const customer = await createCustomer({ name, email });
 
       return redirect(`/sales/customers/${customer.id}`);
@@ -29,6 +54,8 @@ export const action: ActionFunction = async ({ request }) => {
 };
 
 export default function NewCustomer() {
+  const actionData = useActionData() as ActionData;
+
   return (
     <div className="relative p-10">
       <h2 className="mb-4 font-display">New Customer</h2>
@@ -37,12 +64,22 @@ export default function NewCustomer() {
           <label htmlFor="name">
             <LabelText>Name</LabelText>
           </label>
+          {actionData?.errors.name ? (
+            <em id="name-error" className="text-d-p-xs text-red-600">
+              {actionData.errors.name}
+            </em>
+          ) : null}
           <input id="name" name="name" className={inputClasses} type="text" />
         </div>
         <div>
           <label htmlFor="email">
             <LabelText>Email</LabelText>
           </label>
+          {actionData?.errors.email ? (
+            <em id="email-error" className="text-d-p-xs text-red-600">
+              {actionData.errors.email}
+            </em>
+          ) : null}
           <input
             id="email"
             name="email"
@@ -62,6 +99,19 @@ export default function NewCustomer() {
           </button>
         </div>
       </Form>
+    </div>
+  );
+}
+
+export function ErrorBoundary({ error }: { error: Error }) {
+  console.error(error);
+
+  return (
+    <div className="absolute inset-0 flex justify-center bg-red-100 pt-4">
+      <div className="text-center text-red-brand">
+        <div className="text-[14px] font-bold">On snap!</div>
+        <div className="px-2 text-[12px]">There was a problem. Sorry.</div>
+      </div>
     </div>
   );
 }
